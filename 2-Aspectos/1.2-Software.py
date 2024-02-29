b88b0d33bd8f89a2846898b23c30149ed144bab6
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-02-29 11:34:59 -0300

    1.2-Software.py

---
+++ 2-Aspectos/1.2-Software.py
@@ -0,0 +1,27 @@
+title = "Principais Aspectos do Software"
+sub_title = "Definições e características"
+text = '''
+    Software 
+        - Conjunto de intruções
+        - Manipulam dados visando funções e desempenhos adequados
+        - Documentações para operação e uso.
+        - Produto a ser vendido (neste contexto de engenharia)
+        - Exastivamente Testados
+        - Logico não Físico
+        - Engenhosidade X Manofatura
+        - Não Desgasta com o tempo
+        - Sob-Medida
+        - Metodologico
+
+
+        logo, programas domésticos cujo usuário final é o próprio programador não é software
+        nem precisa de documentação. (neste contexto)
+
+        - Por que o software demora tanto para ser concluído?
+        - Por que os custos de produção têm sido tão elevados?
+        - Por que não é possível a detecção de todos erros a priori?
+        - Por que é difícil medir o progresso do desenvolvimento?
+
+
+        
+'''
