77784f774ff3f7108e0bfdde32f64645f19d620f
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-05 11:44:58 -0300

    2.2-Mitos.py

---
+++ 2-Aspectos/2.2-Mitos.py
@@ -0,0 +1,43 @@
+'''
+        - Por que o software demora tanto para ser concluído?
+        - Por que os custos de produção têm sido tão elevados?
+        - Por que não é possível a detecção de todos erros a priori?
+        - Por que é difícil medir o progresso do desenvolvimento?
+        
+'''
+
+
+mito_01 = '''
+    Se a equipe dispõe de um manual repleto de padrões e procedimento de desenvolvimento de software, então a equipe está apta a encaminhar bem o desenvolvimento.
+'''
+
+real_01 = '''
+    Não suficiente, o manual precisa ser exaustivamente completo e condizente com a moderna prática de desenvolvimento, e ainda, precisa ser aplicado efetivamente. 
+'''
+
+mito_02 = '''
+    Computadores de última geração garantem ferramentas de última geração aos desenvolvedores.
+'''
+
+real_02 = '''
+    Mais importante que ferramentas de última geração, são ferramentas eficazem em automatizar procesos de desenvolvimento. 
+'''
+
+mito_03 = '''
+    Para acelerar ou correr com desenvlvimentos atrasados basta aumentar a equipe de desnvolvedores.
+'''
+
+real_03 = '''
+    Inclusão pressupoes treinamento introdutório, logo, uma etapa a mais no processo geral. 
+'''
+
+mito_04 = '''
+    Basta iniciar o projeto, os detalhes sugem e podem ser tratados posteriormente.
+'''
+
+real_04 = '''
+    É preciso muita clareza de objetivos finais: funções, desempenho, interfacs, restrições de projeto e critérios de validação entre outros são cruciais. 
+'''
+
+#pag. 8/145
+#file:///L:/Linear_/Base-TI/Engenharia%20de%20software%20-%20Completa.pdf
